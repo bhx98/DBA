0de1627fc72798799c3241a906b1cf70c0a3c8be
Bahareh Fallahi <39549087+bhx98@users.noreply.github.com>
2026-01-06 09:35:39 +0330

    Add files via upload
    Login to DAC via SQLCMD AND SSMS

---
+++ DAC Login.sql
@@ -0,0 +1,10 @@
+--DAC 
+
+--Connect To DAC Via SSMS
+-->Before Instance Name needs to type Admin:(for example localhost)
+-->Login:sa pass:***
+
+
+-->Connect to DAC via SQLCMD
+sqlcmd -S [server name] -U [user name] -P [password] -A
+
